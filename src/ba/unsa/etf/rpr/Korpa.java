035826088f829c877ad65055b67ab5fdb65ef559
ahusicic1 <ahusicic1@etf.unsa.ba>
2020-10-26 21:17:34 +0100

    Prvi commit, prva verzija klasa

--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -1,4 +1,37 @@
 package ba.unsa.etf.rpr;
 
 public class Korpa {
+    Artikl[] niz = new Artikl[50];
+    int vel=0;
+
+    public boolean dodajArtikl(Artikl a){
+        if(vel==49) return false;
+        niz[vel++] = new Artikl(a.getNaziv(), a.getCijena(), a.getKod());
+        return true;
+    }
+
+    public Artikl[] getArtikli() {
+        return niz;
+    }
+
+    public Artikl izbaciArtiklSaKodom(String kod){
+            int i=0;
+            for(i=0; i<vel; i++){
+                if(niz[i].kod.equals(kod) ){
+                    Artikl a = new Artikl( niz[i].getNaziv(), niz[i].getCijena(), niz[i].getKod());
+                    for(int j=i; j<vel-1 ; j++) niz[j] = niz[j+1];
+                    vel--;
+                    return a;
+                }
+            }
+            return null;
+        }
+
+    public int dajUkupnuCijenuArtikala()   {
+        int cijena=0;
+        for(int i=0; i<vel; i++) cijena = cijena + niz[i].cijena;
+        return cijena;
+    }
+
+
 }
--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -1,4 +1,43 @@
 package ba.unsa.etf.rpr;
 
 public class Supermarket {
+    Artikl[] artikli = new Artikl[1000];
+    int vel=0;
+
+    public Artikl[] getArtikli() {
+        return artikli;
+    }
+
+    public Artikl izbaciArtiklSaKodom(String kod){
+        int i=0;
+        for(i=0; i<vel; i++){
+            if(artikli[i].kod.equals(kod) ){
+                Artikl a = new Artikl(artikli[i].getNaziv(), artikli[i].getCijena(), artikli[i].getKod());
+                for(int j=i; j<vel-1 ; j++) artikli[j] = artikli[j+1];
+                vel--;
+                return a;
+            }
+        }
+        return null;
+    }
+
+    public boolean dodajArtikl(Artikl a){
+        if(vel==999) return false;
+        artikli[vel++] = new Artikl(a.getNaziv(), a.getCijena(), a.getKod());
+        return true;
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
